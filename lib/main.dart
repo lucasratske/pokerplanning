14fe5d35f438c751b15d78806ae4447410010083
Lucas Ratske <lucas.ratske@gmail.com>
2018-08-29 12:38:29 -0300

    Adicionado wrap e dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(new MyApp());
 
@@ -44,19 +45,32 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  void dialog() {
+  void _showDialog(val) {
       showDialog<void>(
         context: context,
         builder: (BuildContext context) {
-          return new Container(
-            alignment: Alignment.center,
-            child: new Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: new Text("0", style: new TextStyle(fontSize: 50.0),),
-            ),
+          return new SimpleDialog(
+            contentPadding: EdgeInsets.all(16.0),
+            children: <Widget>[
+              new Text(
+                val,
+                textAlign: TextAlign.center,
+                style: new TextStyle(fontSize: 50.0, color: Colors.black, decoration: null),
+              ),
+            ],
           );
         }
       );
+  }
+
+  MaterialButton _setButton(val) {
+    return new MaterialButton(
+      child: new Text(val, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+      color: Colors.purple,
+      textColor: Colors.white,
+      onPressed: () => _showDialog(val),
+      padding: EdgeInsets.all(10.0),
+    );
   }
 
   @override
@@ -68,17 +82,30 @@ class _MyHomePageState extends State<MyHomePage> {
       body: new Padding(
         padding: const EdgeInsets.all(16.0),
         child: new Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget> [ //Padding between these please
-                new MaterialButton(
-                  child: new Text("0", style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
-                  color: Colors.lightBlue,
-                  textColor: Colors.white,
-                  onPressed: dialog,
-                )
-              ]
-            )
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget> [
+            new Wrap(
+              spacing: 8.0, // gap between adjacent chips
+              runSpacing: 4.0, // gap between lines
+              direction: Axis.horizontal,
+              children: <Widget>[
+                _setButton("0"),
+                _setButton("1"),
+                _setButton("2"),
+                _setButton("3"),
+                _setButton("5"),
+                _setButton("8"),
+                _setButton("13"),
+                _setButton("21"),
+                _setButton("34"),
+                _setButton("55"),
+                _setButton("89"),
+                _setButton("144"),
+              ],
+            )   
+          ]
+        )
       ),
     );
   }
